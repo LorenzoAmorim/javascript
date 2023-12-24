7abc9f29ddb3573b72200221faa5e02eff66169c
Lorenzo Amorim <lorenzoamorim@hotmail.com>
2023-12-24 14:38:36 -0300

    if, else versão 2 dos estudos

---
+++ v2-aula13-ifelse/ifelse.js
@@ -0,0 +1,34 @@
+/**
+ *  BLOCOS DE CÓDIGO QUE IRÃO SER EXECUTADOS SOMENTE QUANDO UMA DETERMINADA
+ * OPREÇÃO LÓGICA FOR VERDADEIRA
+ */
+
+const idade = prompt("Qual a sua idade?")
+
+if(idade => 18){
+    alert("Você é maior de idade")
+} else if(idade > 12){
+    alert("Você é menor de idade")
+}else if(idade > 4){
+    alert("Você é criança")
+}else("")
+
+// OPERADOR TERNÁRIO
+// ####################
+    // const resultado = (condicao) ? resultado_verdadeiro : resultado_falso
+    //"?"" = se, ":" = senão
+
+    /*const idade = prompt("Qual a sua idade?")
+
+    const resultado = (idade >18) ? "Você é maior de idade" : "Você é criança"
+
+    console.log(resultado)*/
+
+
+//condição teste
+
+/*if(idade == 6){
+    console.log("Verdadeiro")
+}else ( 
+    console.log ("KKKKKKKKKKKKK")
+)*/    
