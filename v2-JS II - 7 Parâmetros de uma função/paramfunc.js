05b508d003b80496de3e02f7cac3fb5a1166f62a
Lorenzo Amorim <lorenzoamorim@hotmail.com>
2023-12-27 23:03:22 -0300

    continuação estudos parâmetro das funções - V2

--- v2-JS II - 7 Parâmetros de uma função/paramfunc.js
+++ v2-JS II - 7 Parâmetros de uma função/paramfunc.js
@@ -17,4 +17,29 @@ function soma(a, b){
     valor = a +b
     alert("O valor das somas são " + valor)
 }
-soma(15,1)
+//soma(15,1)
+
+function criarUsuario(nome, email, senha, tipo){
+    const usuario = {
+        nome, // ou apenas "nome," pois os atributos e os parâmetros são iguais
+        email,
+        senha,
+        tipo
+    }
+    //console.log(usuario)
+}
+
+//criarUsuario("Jorge L Castro", "Castrobb49150@hotmail.com", "1234", "admin")
+
+//valores padrões sempre devem ser os últimos nos parâmetros.
+function novoUsuario(nome, email, senha, tipo = "admin"){
+    const usuario = {
+        nome,
+        email,
+        senha,
+        tipo
+    }
+    console.log(usuario)
+}
+//atributo "tipo" não definido abaixo
+novoUsuario("Jorge", "castrinho@hothot", "112")
